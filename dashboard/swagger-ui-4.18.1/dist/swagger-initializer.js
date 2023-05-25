61fec33cfb102f00ec1a439cf637f25d3f8a5146
wdhwang <wendwo.hwang@gmail.com>
2023-05-25 11:58:28 +0800

    Update swagger-initializer.js

--- dashboard/swagger-ui-4.18.1/dist/swagger-initializer.js
+++ dashboard/swagger-ui-4.18.1/dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://10.50.0.22:30002/swagger/APISIX_routes.yaml",
+    url: "http://10.50.0.22:30002/swagger/APISIX_routes.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
